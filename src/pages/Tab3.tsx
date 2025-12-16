ad9a792bac1805a9031a99efb504f3c509e70b3f
Usuario invitado <Guest@iMacdePCEA10810.PUCE.EDU.EC>
2025-12-16 16:12:04 -0500

    Cambios clase 16/12/2025

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,8 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import { useState } from 'react';
+import { UserInfo } from '../interfaces/UserInfo';
 import './Tab3.css';
+import { getUserInfo } from '../services/GithubService';
+
 
 const Tab3: React.FC = () => {
+
+  const [userInfo, setUserInfo] = useState<UserInfo>({
+    name: "Usuario no encontrado",
+    login: "no-username",
+    bio: "No se encuentra usuario",
+    avatar_url: "https://i.pinimg.com/236x/02/af/ac/02afac9a3b1b51819495d2b142c452c6.jpg"
+  })
+
+  const loadUserInfo = async () => {
+    const response = await getUserInfo();
+    if (response) {
+      setUserInfo(response);
+    }
+  };
+
+  useIonViewDidEnter( () =>  {
+    loadUserInfo();
+  });
+
   return (
     <IonPage>
       <IonHeader>
@@ -18,12 +41,12 @@ const Tab3: React.FC = () => {
         </IonHeader>
         <div className="card-container">
           <IonCard className="card">
-            <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+            <img alt="Silhouette of mountains" src={userInfo.avatar_url} />
             <IonCardHeader>
-              <IonCardTitle>Ariel Arcentales</IonCardTitle>
-              <IonCardSubtitle>elArielArcentales</IonCardSubtitle>
+              <IonCardTitle>{userInfo.name}</IonCardTitle>
+              <IonCardSubtitle>{userInfo.login}</IonCardSubtitle>
             </IonCardHeader>
-            <IonCardContent>Este es el perfil de Ariel Arcentales</IonCardContent>
+            <IonCardContent>{userInfo.bio}</IonCardContent>
           </IonCard>
         </div>
       </IonContent>
--- src/services/GithubService.ts
+++ src/services/GithubService.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { RepositoryItem } from "../interfaces/RepositoryItem";
+import { UserInfo } from "../interfaces/UserInfo";
 
-const GITHUB_API_URL = "https://api.github.com";
+const GITHUB_API_URL = import.meta.env.VITE_GITHUB_API_URL;
 const GITHUB_API_TOKEN = import.meta.env.VITE_GITHUB_API_TOKEN; // sin 'Bearer' aquí
 
 export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
@@ -58,3 +59,18 @@ export const createRepository = async (name: string, description: string): Promi
     console.error("Error creating repository:", error.response?.data || error.message);
   }
 };
+
+export const getUserInfo = async(): Promise<UserInfo | null> => {
+  try {
+    const response = await axios.get(`${GITHUB_API_URL}/user`, {
+      headers: {
+        Authorization: `Bearer ${GITHUB_API_TOKEN}`
+      }
+    });
+    return response.data;
+  }catch (error) {
+    console.error("Error recuperando usuario:", error)
+    alert("Error recuperando usuario")
+    return null
+  }
+}
